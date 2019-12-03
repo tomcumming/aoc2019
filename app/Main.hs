6786f9043e3c35c4f7a524665633bfc0779e7b3d
Tom Cumming <mail@tomcumming.co.uk>
2019-12-03 18:04:01 +0000

    Day 2

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,14 @@ module Main where
 import System.Environment (getArgs)
 
 import Day1 (day1)
+import Day2 (day2)
 
 main :: IO ()
 main = do
     args <- getArgs
     contents <- getContents
     let result = case args of
-                    ("1":_) -> day1 contents
+                    ["1"] -> day1 contents
+                    ["2"] -> day2 contents
                     _ -> "Specify a day"
     putStrLn result
